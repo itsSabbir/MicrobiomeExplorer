4bd69b25ccb3447213885e14eee26e956ef625f5
itsSabbir <53710832+itsSabbir@users.noreply.github.com>
2023-11-13 17:38:18 -0500

    added the LICENSE and changed the previous version of the LICENSE file to be .md format. Added tests for the two functions in the package

---
+++ tests/testthat/testcalculate_stats.R
@@ -0,0 +1,20 @@
+library(testthat)
+library(MicrobiomeExplorer)
+
+# Test calculate_stats with valid data
+test_that("calculate_stats returns correct results with valid data", {
+  test_data <- data.frame(
+    Taxa1 = c(1, 2, 3, 4, 5),
+    Taxa2 = c(2, 3, 4, 5, 6)
+  )
+  result <- calculate_stats(test_data)
+  expect_true(all(colnames(result) == c("mean", "median", "sd", "variance", "range_min", "range_max", "IQR")))
+  expect_equal(result$mean, c(3, 4))
+  expect_equal(result$median, c(3, 4))
+})
+
+# Test calculate_stats with empty data
+test_that("calculate_stats handles empty data", {
+  test_data <- data.frame()
+  expect_error(calculate_stats(test_data), "Data must have at least one column.")
+})
---
+++ tests/testthat/testplot_microbiome_heatmap.R
@@ -0,0 +1,15 @@
+library(testthat)
+library(MicrobiomeExplorer)
+
+# Test plot_microbiome_heatmap with valid data
+test_that("plot_microbiome_heatmap creates a heatmap object", {
+  test_data <- matrix(rnorm(20), nrow = 5)
+  result <- plot_microbiome_heatmap(test_data)
+  expect_is(result, "Heatmap")
+})
+
+# Test plot_microbiome_heatmap with non-numeric data
+test_that("plot_microbiome_heatmap handles non-numeric data", {
+  test_data <- data.frame(Taxa1 = c("A", "B", "C"), Taxa2 = c("D", "E", "F"))
+  expect_error(plot_microbiome_heatmap(test_data))
+})
